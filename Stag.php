3d0206d825b6d9a5244df100b45af3a4d19611ff
Ondřej Henek <hello@ondrejhenek.cz>
2016-08-16 18:22:39 +0200

    General update of many features

--- Stag.php
+++ Stag.php
@@ -1,6 +1,8 @@
 <?php
 namespace Faculty;
 
+use Exception;
+
 /**
  * 
  *
@@ -8,22 +10,34 @@ namespace Faculty;
 class Stag
 {
 
-	protected function loginUrl ($redirectUrl)
+	static public function getAcademicYear($date = null) {
+		$timestamp = !empty($date) ? strtotime($date) : time();
+		return date('Y', $timestamp - 20736000);
+		// UPDATE items SET `year` =  DATE_SUB(`date`, INTERVAL 20736000 SECOND) WHERE year IS NULL
+	}
+
+	/**
+	 * Gets semester of given date
+	 * From 31. 1. to 3. 7. is LS then ZS
+	 * @return string LS/ZS
+	 */
+	static public function getSemester($date = null) {
+		$timestamp = !empty($date) ? strtotime($date) : time();
+		$day = date('z', $timestamp) + 1;
+		if ($day > 30 && $day < 210) {
+			return 'LS';
+		} else {
+			return 'ZS';
+		}
+	}
+
+	static public function loginUrl ($redirectUrl)
 	{
 		return 'https://stagservices.upol.cz/ws/login?originalURL='. $redirectUrl;
 	}
 
-	public function facultyLogin()
-	{
-		
-	}
 
-	public function syncCourses()
-	{
-
-	}
-
-
+/*
 	public function verify($stagUserTicket, $user_id) {
 
 		$ticketData = $this->getStagIdentityByTicket($stagUserTicket);
@@ -68,16 +82,20 @@ class Stag
 		return true;
 
 	}
+*/
 
-	public function getStagIdentityByTicket($stagUserTicket) {
-		// test ticket validity
 
-		$identities = $this->getIdentityByTicket($stagUserTicket);
+	/**
+	 * Tests ticket validity
+	 * 
+	 * @throws Exception - not a valid ticket
+	 * @return 
+	 */
+	static public function getStagIdentityByTicket($stagUserTicket, $stagLoginLetter) {
+		$identities = Stag::getIdentityByTicket($stagUserTicket);
 		if ($identities == null) {
 			throw new Exception('Tvoje přihlášení do STAGu již vypršelo. Zkus to prosím znovu.');
 		}
-
-		// TOHLE ULOZ!!!! TADYHLE! DATA! VOLE!
 
 		$osobniCislo = false;
 		$role = null;
@@ -90,7 +108,7 @@ class Stag
 				break;
 			}
 			if ($stagUser->role == 'ST') {
-				if (preg_match('/'. Configure::read('Faculty.stagLoginLetter') .'\d*/', $stagUser->userName)) {
+				if (preg_match('/'. $stagLoginLetter .'\d*/', $stagUser->userName)) {
 					$osobniCislo = $stagUser->userName;
 					$role = 'ST';
 					break;
@@ -101,8 +119,12 @@ class Stag
 		return array('id' => $osobniCislo, 'role' => $role, 'ucitidno' => $ucitidno);
 	}
 
-	public function getStudentInfoFromStag($osobniCislo) {
-		$student = $this->getStudentInfoByOsobniCislo($osobniCislo);
+	/**
+	 * Fetches student data from Stag based on osobni cislo
+	 *
+	 */
+	static public function getStudentInfo($osobniCislo) {
+		$student = Stag::getStudentInfoByOsobniCislo($osobniCislo);
 
 		$data = array(
 			'upol_osobni_cislo' => $osobniCislo,
@@ -112,13 +134,17 @@ class Stag
 			'surname' => mb_convert_case($student->prijmeni, MB_CASE_TITLE),
 			'obor_id' => $student->oborIdnos, // POZOR! PRO VICE OBORU tu jsou 2 cisla oddelena carkou!
 			'rocnik' => $student->rocnik,
-			'upol_verified' => date("Y-m-d H:i:s"),
+			// 'upol_verified' => date("Y-m-d H:i:s"),
 		);
 		return $data;
 	}
 
-	public function getTeacherInfoFromStag($ucitidno) {
-		$teacher = $this->getTeacherInfoByUcitidno($ucitidno);
+	/**
+	 * Fetches teacher data from Stag based on ucitIdNo
+	 *
+	 */
+	static public function getTeacherInfo($ucitidno) {
+		$teacher = Stag::getTeacherInfoByUcitidno($ucitidno);
 		
 		$data = array(
 			'name' => $teacher->jmeno,
@@ -126,7 +152,7 @@ class Stag
 			'fullname' => $teacher->titulPred .' '. $teacher->jmeno .' '. $teacher->prijmeni .' '. $teacher->titulZa,
 			'email' => $teacher->email,
 			'ucitidno' => $teacher->ucitIdno,
-			'upol_verified' => date("Y-m-d H:i:s"),
+			// 'upol_verified' => date("Y-m-d H:i:s"),
 		);
 		return $data;
 	}
@@ -211,8 +237,7 @@ class Stag
 		$student = json_decode($result)[0];
 
 		if (!property_exists($student, 'userName')) {
-			dbg($result);
-			throw new Exception('STAGu? ...jsi tam? :( Vypadá to na chybu v komunikaci. Nahlas prosím tento problém ať to co nejdříve opravíme. Díky!');
+			throw new Exception('Bohužel jsme tě ve STAGu nenašli. Napiš nám a zkontrolujeme to spolu.');
 		}
 
 		return $student;
@@ -236,8 +261,9 @@ class Stag
 *     'url' => NULL,
 *  }
 **/
-	static public function getTeacherInfoByUcitidno($ucitidno) {
+	static public function getTeacherInfoByUcitidno ($ucitidno) {
 		$result = file_get_contents('https://stagservices.upol.cz/ws/services/rest/ucitel/getUcitelInfo?outputFormat=JSON&ucitIdno='. $ucitidno);
+		if (!$result) throw new Exception("Bohužel se nám nepodařilo spojit se STAGem.");
 		return json_decode($result)[0];
 	}
 
@@ -247,9 +273,94 @@ class Stag
 *     
 *  }
 **/
-	static public function getCoursesByUcitidno($ucitidno) {
-		$result = file_get_contents('https://stagservices.upol.cz/ws/services/rest/predmety/getPredmetyByUcitel?outputFormat=JSON&ucitIdno='. $ucitidno);
-		return json_decode($result)[0];
+	static public function getCoursesForTeacher ($ucitidno, $semester) {
+		$courses = file_get_contents('https://stagservices.upol.cz/ws/services/rest/predmety/getPredmetyByUcitel?outputFormat=JSON&ucitIdno='. $ucitidno .'&semestr='. $semester);
+
+		if (!$courses) throw new Exception('Server UPOL neodpovídá. Zkus si synchronizovat předměty za chvíli.');
+
+		return json_decode($courses, true)[0]['predmetUcitele'];
+	}
+
+
+	/**
+	 * Ziska ze STAGu seznam predmetu studenta podle zadaneho osobniho cisla a semestru (LS / ZS)
+	 *
+	 */
+	static public function getCoursesForStudent ($osobniCislo, $semester)
+	{
+		$courses = file_get_contents('https://stagservices.upol.cz/ws/services/rest/predmety/getPredmetyByStudent?osCislo='. $osobniCislo .'&semestr='. $semester .'&outputFormat=JSON');
+		// $result = file_get_contents(TMP . DS . 'predmety_john.json');
+
+		if (!$courses) throw new Exception('Server UPOL neodpovídá. Zkus si synchronizovat předměty za chvíli.');
+		
+		return json_decode($courses, true)[0]['predmetStudenta'];
+	}
+
+
+	/**
+	 * Sends login info, checks ticket and responds with $user
+	 */
+	static public function sendLogin ($data, $debug = false)
+	{
+		$url = Stag::loginUrl('http://www.entoo.cz');
+        $data = array('username' => $data['username'], 'password' => $data['password'], 'submit' => 'Přihlásit se', 'loginMethod' => 'jaas', 'originalURL' => 'http://www.entoo.cz');
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+
+        if ($debug) {
+	        curl_setopt($curl, CURLOPT_VERBOSE, true);
+	        $verbose = fopen('php://temp', 'w+');
+	        curl_setopt($curl, CURLOPT_STDERR, $verbose);
+	    }
+
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        if ($debug) {
+	        debug($response);
+	        rewind($verbose);
+	        $verboseLog = stream_get_contents($verbose);
+	        echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
+	    }
+
+	    // pokud tam neni redirect s ticketem, tak se to nepovedlo... FALSE!
+        $pattern = '/stagUserTicket=(.*?)&/';
+        if (!preg_match($pattern, $response, $ticket)) {
+            return false;
+        }
+
+       	return $ticket[1];
+	}
+
+
+	/**
+	 * Ziska ze STAGu seznam studijnich programu podle fakulty
+	 *
+	 */
+	static public function getPrograms ($faculty = 'PFA')
+	{
+		$programs = file_get_contents('https://stagservices.upol.cz/ws/services/rest/programy/getStudijniProgramy?kod=%25&fakulta='. $faculty .'&outputFormat=json');
+
+		if (!$programs) throw new Exception('Server UPOL neodpovídá. Možná je položen špatný dotaz.');
+		
+		return json_decode($programs, true)[0]['programInfo'];
+	}
+
+	/**
+	 * Ziska ze STAGu seznam studijnich oboru podle programu
+	 *
+	 */
+	static public function getFields ($programId)
+	{
+		$fields = file_get_contents('https://stagservices.upol.cz/ws/services/rest/programy/getOboryStudijnihoProgramu?outputFormat=json&stprIdno='. $programId);
+
+		if (!$fields) throw new Exception('Server UPOL neodpovídá. Možná je položen špatný dotaz.');
+		
+		return json_decode($fields, true)[0]['oborInfo'];
 	}
 
 
